f403459bd6c781cd3fd992bd7e69a72835c16cfe
Eduardo <juantokx@gmail.com>
2025-06-19 14:59:09 -0600

    Basic route setup for auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React, { useState } from 'react'
+
+const page = () => {
+
+    const [first, setfirst] = useState(1)
+
+  return (
+    <div>Dashboad</div>
+  )
+}
+
+export default page
