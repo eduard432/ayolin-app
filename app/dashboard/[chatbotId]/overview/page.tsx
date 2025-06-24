67f8f5f97624d5f5e3c97ab5e3aeb73a4d87fe24
Eduardo <juantokx@gmail.com>
2025-06-23 18:50:03 -0600

    Ejemplo dynamic route

---
+++ app/dashboard/[chatbotId]/overview/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// /dashboard/jenfjnefuy348u3843/overview
+const Page = ({params}: {params: {chatbotId: string}}) => {
+
+
+
+  return (
+    <div>page</div>
+  )
+}
+
+export default Page
