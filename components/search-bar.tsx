af56c9591d1b9ef8eae61b38d09f3e84d4cf1492
Eduardo <juantokx@gmail.com>
2025-06-23 19:37:42 -0600

    SearchBar component

---
+++ components/search-bar.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+import { Search } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+
+interface SearchBarProps {
+	className?: string
+	placeholder?: string
+	value?: string
+	onChange?: React.ChangeEventHandler<HTMLInputElement>
+}
+
+export const SearchBar = ({ className, placeholder = "Search", value, onChange }: SearchBarProps) => {
+	return (
+		<div className={cn('w-full relative', className)}>
+			<Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500" />
+			<Input type="text" placeholder={placeholder} value={value} onChange={onChange} className="pl-8" />
+		</div>
+	)
+}
