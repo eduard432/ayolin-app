7b32a6d3c57a89c3e7c8afa2fcba57e3cfb0198e
LeoDuffed <carlosmroder@gmail.com>
2025-06-23 23:19:37 -0600

    nada

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 export default function Navbar() {
 
   return (
-    <nav id="navbar" className="fixed top-0 left-0 w-full h-16 bg-neutral-900 ">
+    <nav id="navbar" className="fixed top-0 left-0 w-full h-16 bg-neutral-900 z-50">
         <div className="container mx-auto px-4 py-4 flex justify-between items-center h-full">
             <Link href="/" className="text-3xl font-bold text-sky-700">AYOLIN</Link>
         </div>
