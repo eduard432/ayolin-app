3823c74781bd754e586399b05ce490f88c08e314
LeoDuffed <carlosmroder@gmail.com>
2025-06-25 12:25:33 -0600

    cambios en el layout de auth

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({children} : {children: React.ReactNode}) => {
     return (
-        <div className="h-[800px] flex items-center justify-center bg-neutral-200">
+        <div className="min-h-screen flex items-center justify-center bg-neutral-200">
             {children}
         </div>
     );
