70d3e809469ee20259673531c3460b81f3f73f9b
eduard432 <juantokx@gmail.com>
2025-06-25 09:31:04 -0600

    refactor layout component to handle session authentication and redirect

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,14 +2,23 @@ import React from 'react'
 import NavbarDashboard from '@/components/navbarDashboard'
 import Navbar from '@/components/navbar'
 import { SessionProvider } from 'next-auth/react'
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation'
 
-const layout = ({
+const layout = async ({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) => {
+	const session = await auth()
+	if (!session) redirect('/login')
+
 	return (
-		<SessionProvider basePath="/api/v1/auth">
+		<SessionProvider
+			refetchInterval={60 * 60 * 6}
+			session={session}
+			basePath="/api/v1/auth"
+		>
 			<div className="bg-neutral-200 min-h-screen">
 				<Navbar />
 				<NavbarDashboard />
