b92f0136d585993e881b9ab914c66143d419b952
LeoDuffed <carlosmroder@gmail.com>
2025-06-25 01:45:16 -0600

    nada

--- app/dashboard/[chatbotId]/almacenamiento/page.tsx
+++ app/dashboard/[chatbotId]/almacenamiento/page.tsx
@@ -20,7 +20,7 @@ export default function AlmacenamientoPage() {
         id.replace(/-/g, ' ').replace(/\b\w/g, (l) => l.toUpperCase())
 
     return(
-        <div className='px-15 space-y-6 pt-8'>
+        <div className='px-15 space-y-6 pt-8 pb-16'>
             <div>
                 <h1 className='text-4xl font-semibold tracking-tight text-black'>
                     Almacenamiento de {formatName(chatbotId)}
