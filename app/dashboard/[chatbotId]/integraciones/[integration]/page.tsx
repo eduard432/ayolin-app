59f574386de135c761c2b9e18bdaeaeafee41522
eduard432 <juantokx@gmail.com>
2025-06-26 20:26:08 -0600

    Add integration page and UI components for integrations

---
+++ app/dashboard/[chatbotId]/integraciones/[integration]/page.tsx
@@ -0,0 +1,77 @@
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Page = async ({
+	params,
+}: {
+	params: Promise<{ integration: string }>
+}) => {
+	const { integration } = await params
+
+	return (
+		<div className="grid grid-cols-12 px-44 gap-x-16 gap-y-4 pb-24">
+			<section className="flex justify-between col-span-12">
+				<h4 className="scroll-m-20 text-3xl font-semibold tracking-tight mb-4 capitalize">
+					{integration}
+				</h4>
+				<Button>Install</Button>
+			</section>
+			<section className="col-span-3 space-y-6">
+				<AspectRatio ratio={16 / 9} className="bg-muted rounded-lg">
+					<Image
+						src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+						alt="Photo by Drew Beamer"
+						fill
+						className="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale"
+					/>
+				</AspectRatio>
+				<p className="text-sm">
+					Descripción de la integración resumida en una frase
+				</p>
+				<div>
+					<h5 className="font-semibold">Category</h5>
+					<Badge variant="secondary" className="bg-neutral-200" >Analíticas</Badge>
+				</div>
+				<div>
+					<h5 className="font-semibold">Installs</h5>
+					<p className="text-sm" >500+ installs</p>
+				</div>
+				<div>
+					<h5 className="font-semibold">Resources</h5>
+					<Button asChild variant="link" className="text-blue-500 text-sm px-0 block font-normal" >
+						<Link href="/" >Documentation</Link>
+					</Button>
+					<Button asChild variant="link" className="text-blue-500 text-sm px-0 block font-normal" >
+						<Link href="/" >EULA</Link>
+					</Button>
+					<Button asChild variant="link" className="text-blue-500 text-sm px-0 block font-normal" >
+						<Link href="/" >Privacy Policy</Link>
+					</Button>
+					<Button asChild variant="link" className="text-blue-500 text-sm px-0 block font-normal" >
+						<Link href="/" >Website</Link>
+					</Button>
+				</div>
+			</section>
+			<section className="col-span-9 space-y-4">
+				<AspectRatio ratio={16 / 9} className="bg-muted rounded-lg">
+					<Image
+						src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+						alt="Photo by Drew Beamer"
+						fill
+						className="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale"
+					/>
+				</AspectRatio>
+				<div className="space-y-2" >
+					<h3 className="text-lg font-semibold" >Overview</h3>
+					<p className="text-neutral-800" >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ea ipsa vitae nam sint, corporis magni dolorum ducimus molestias hic totam quis eaque tempore quae unde. Magnam magni distinctio veniam at aliquam pariatur debitis, facilis quia incidunt, iure, possimus neque placeat. Libero iure eius asperiores illum dolorem, consequuntur fuga non!</p>
+				</div>
+			</section>
+		</div>
+	)
+}
+
+export default Page
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // For image testing:
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
