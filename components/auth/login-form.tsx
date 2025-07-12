837ee279329eb358efa21f816c3101714ce7f091
LeoDuffed <carlosmroder@gmail.com>
2025-07-11 18:13:11 -0600

    nada imp

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -64,7 +64,7 @@ export const LoginForm = () => {
 					setShowTwoFactor(true)
 				}
 			})
-			.catch(() => setError("Algo salio mal"))
+			{/*.catch(() => setError("Algo salio mal"))*/}
 		})
 	}
 
