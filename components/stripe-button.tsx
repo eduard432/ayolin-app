e43efe6f3e4394f30550a33375076fa660c87e62
LeoDuffed <carlosmroder@gmail.com>
2025-07-16 18:30:11 -0600

    Actualizar enlace en el botón de pago de Stripe para redirigir a la página correcta

--- components/stripe-button.tsx
+++ components/stripe-button.tsx
@@ -14,7 +14,7 @@ export function PayWithStripe( {...props } : React.ComponentProps<"button"> & Va
 
   return (
     <Button {...props} asChild>
-      <Link href="/dashboard/planes" rel="noopener noreferrer">
+      <Link href="/bohayaun" rel="noopener noreferrer">
         Únete
       </Link>
     </Button>
