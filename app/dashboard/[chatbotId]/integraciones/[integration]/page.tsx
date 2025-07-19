4a08dd1672b466044c8bf57544b42a0da7867764
eduard432 <juantokx@gmail.com>
2025-07-19 08:58:45 -0600

    Refactor IntegracionesPage layout for improved responsiveness and add navigation link

--- app/dashboard/[chatbotId]/integraciones/[integration]/page.tsx
+++ app/dashboard/[chatbotId]/integraciones/[integration]/page.tsx
@@ -2,7 +2,9 @@ import { InstallToolButton } from '@/components/InstallButton'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
 import { getToolFunctionByKeyName } from '@/data/tools.server'
+import { ArrowLeft } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
@@ -11,45 +13,41 @@ import React from 'react'
 const Page = async ({
 	params,
 }: {
-	params: Promise<{ integration: string }>
+	params: Promise<{ integration: string; chatbotId: string }>
 }) => {
-	const { integration } = await params
+	const { integration, chatbotId } = await params
 	const toolFunction = await getToolFunctionByKeyName(integration)
 
 	if (!toolFunction) return notFound()
 
 	return (
-		<div className="grid grid-cols-12 px-44 gap-x-16 gap-y-4 pb-24">
-			<section className="flex justify-between col-span-12">
+		<div className="grid grid-cols-1 md:grid-cols-12 mx-auto w-full md:w-9/12 gap-x-16 gap-y-4 pb-8">
+			<section className="col-span-full">
+				<Link
+					className="flex items-center gap-x-2 text-muted-foreground"
+					href={`/dashboard/${chatbotId}/integraciones/marketplace`}
+				>
+					<ArrowLeft className="h-5 w-5" /> Regresar a marketplace
+				</Link>
+			</section>
+			<section className="flex justify-between col-span-full">
 				<h4 className="scroll-m-20 text-3xl font-semibold tracking-tight mb-4 capitalize">
 					{toolFunction.name}
 				</h4>
 				<InstallToolButton />
 			</section>
-			<section className="col-span-3 space-y-6">
-				<AspectRatio ratio={16 / 9} className="bg-muted rounded-lg">
-					<Image
-						src={toolFunction.imageUrl}
-						alt={`Tool Function image for ${toolFunction.name}`}
-						fill
-						className="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale"
-					/>
-				</AspectRatio>
-				<p className="text-sm">
-					Descripción de la integración resumida en una frase
-				</p>
+			<Separator className="col-span-full" />
+			<section className="col-span-full md:col-span-3 space-y-6 hidden md:block">
 				<div>
-					<h5 className="font-semibold">Category</h5>
-					<Badge variant="secondary" className="bg-neutral-200">
-						Analíticas
-					</Badge>
+					<h5 className="font-semibold">Categoría:</h5>
+					<Badge variant="secondary">Analíticas</Badge>
 				</div>
 				<div>
 					<h5 className="font-semibold">Installs</h5>
 					<p className="text-sm">500+ installs</p>
 				</div>
 				<div>
-					<h5 className="font-semibold">Resources</h5>
+					<h5 className="font-semibold">Recursos</h5>
 					<Button
 						asChild
 						variant="link"
@@ -80,7 +78,11 @@ const Page = async ({
 					</Button>
 				</div>
 			</section>
-			<section className="col-span-9 space-y-4">
+			<section className="col-span-full md:col-span-9 space-y-4">
+				<div className="space-y-2">
+					<h3 className="text-lg font-semibold">Overview</h3>
+					<p className="text-primary">{toolFunction.blogDescription}</p>
+				</div>
 				<AspectRatio ratio={16 / 9} className="bg-muted rounded-lg">
 					<Image
 						src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
@@ -89,10 +91,6 @@ const Page = async ({
 						className="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale"
 					/>
 				</AspectRatio>
-				<div className="space-y-2">
-					<h3 className="text-lg font-semibold">Overview</h3>
-					<p className="text-primary">{toolFunction.blogDescription}</p>
-				</div>
 			</section>
 		</div>
 	)
