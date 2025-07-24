d9d3cb21917a3b600cb113acbc3ef744d634a733
eduard432 <juantokx@gmail.com>
2025-07-24 13:17:49 -0600

    fix: adding loading state

--- app/dashboard/[chatbotId]/integraciones/marketplace/page.tsx
+++ app/dashboard/[chatbotId]/integraciones/marketplace/page.tsx
@@ -11,6 +11,7 @@ import {
 	CardContent,
 	CardHeader,
 } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
 import { useChatbot } from '@/data/chatbot.client'
 
 import { useIntegrations } from '@/data/integrations.client'
@@ -21,16 +22,42 @@ import React from 'react'
 
 const channels = ['telegram']
 
+const IntegrationCardSkeleton = () => {
+	return (
+		<Card className="pt-0 justify-start relative">
+			<CardHeader className="absolute right-20 top-4 z-10">
+				<Skeleton className="h-9 w-20" />
+			</CardHeader>
+
+			<AspectRatio
+				ratio={16 / 9}
+				className={cn('bg-muted rounded-lg rounded-b-none')}
+			>
+				<Skeleton className="h-full w-full rounded-lg rounded-b-none" />
+			</AspectRatio>
+
+			<CardContent>
+				<Skeleton className="h-5 w-3/4 mb-2" />
+				<Skeleton className="h-4 w-full" />
+			</CardContent>
+		</Card>
+	)
+}
+
 const IntegrationsPage = () => {
 	const { data } = useIntegrations()
 	const router = useRouter()
 	const params = useParams()
 	const chatbotId = params?.chatbotId as string
 
-	const { data: chatbot } = useChatbot(chatbotId)
+	const { isLoading, data: chatbot } = useChatbot(chatbotId)
 
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+			{isLoading &&
+				Array.from({ length: 3 }).map((_, i) => {
+					return <IntegrationCardSkeleton key={i} />
+				})}
 			{data &&
 				chatbot &&
 				data.map((integration) => (
