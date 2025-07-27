4013e23013eb24464de93ee1ac826bf2b4530e28
eduard432 <juantokx@gmail.com>
2025-07-27 13:46:16 -0600

    fix: Splitted code

---
+++ app/dashboard/[chatbotId]/editar/EditChatbotForm.tsx
@@ -0,0 +1,211 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import {
+	Select,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
+import { Textarea } from '@/components/ui/textarea'
+
+import { updateChatbot } from '@/data/chatbot.client'
+import { modelPrices } from '@/lib/constants/models'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Chatbot } from '@prisma/client'
+import { SelectContent } from '@radix-ui/react-select'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+const formSchema = z.object({
+	name: z.string(),
+	initialPrompt: z.string(),
+	model: z.string(),
+})
+
+const EditChatbotForm = ({ chatbot }: { chatbot: Chatbot }) => {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: chatbot.name,
+			initialPrompt: chatbot.initialPrompt,
+			model: chatbot.model,
+		},
+	})
+
+	const queryClient = useQueryClient()
+
+	const mutation = useMutation<
+		Chatbot,
+		Error,
+		{
+			chatbotId: string
+			data: z.infer<typeof formSchema>
+		},
+		{ previousChatbot: Chatbot }
+	>({
+		mutationFn: async (data) => {
+			const result = await updateChatbot(data.chatbotId, data.data)
+			return result
+		},
+		onError: (_, data, context) => {
+			toast.error('Error updating chatbot')
+			queryClient.setQueryData(
+				['chatbot', data.chatbotId],
+				context?.previousChatbot
+			)
+		},
+		onMutate: async (data) => {
+			await queryClient.cancelQueries({
+				queryKey: ['chatbot', data.chatbotId],
+			})
+
+			const previousChatbot = queryClient.getQueryData<Chatbot>([
+				'chatbot',
+				data.chatbotId,
+			]) as Chatbot
+
+			queryClient.setQueryData(['chatbot', data.chatbotId], (old: Chatbot) => {
+				if (!old) return old
+				return {
+					...old,
+					...data.data,
+				}
+			})
+
+			return { previousChatbot }
+		},
+		onSuccess: () => {
+			toast.success('Chatbot updated successfully')
+		},
+	})
+
+	const onSubmit = (values: z.infer<typeof formSchema>) => {
+		if (chatbot) {
+			mutation.mutate({ data: values, chatbotId: chatbot.id })
+		}
+	}
+
+	return (
+		<Card className="md:w-1/2 md:mx-auto">
+			<CardHeader>
+				<CardTitle className="text-xl">Editar {chatbot.name}</CardTitle>
+				<CardDescription>
+					Crate a chatbot with your own custom settings
+				</CardDescription>
+				<CardAction>
+					<Button
+						disabled={!form.formState.isDirty}
+						onClick={() => form.reset()}
+						size="sm"
+					>
+						Reset
+					</Button>
+				</CardAction>
+			</CardHeader>
+			<CardContent>
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+						<FormField
+							control={form.control}
+							name="name"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormDescription>Chatbot public name</FormDescription>
+									<FormControl>
+										<Input placeholder="Chatcito" {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="model"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Model</FormLabel>
+									<FormDescription>AI model for chatbot</FormDescription>
+									<Select
+										onValueChange={field.onChange}
+										value={field.value}
+										disabled
+									>
+										<FormControl>
+											<SelectTrigger className="w-full">
+												<SelectValue placeholder="Select a model" />
+											</SelectTrigger>
+										</FormControl>
+										<SelectContent>
+											{modelPrices.map((model) => (
+												<SelectItem
+													disabled={model.name !== 'gpt-4.1-nano'}
+													key={model.name}
+													value={model.name}
+												>
+													{model.name}
+												</SelectItem>
+											))}
+										</SelectContent>
+									</Select>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="initialPrompt"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Initial Prompt</FormLabel>
+									<FormDescription>
+										Prompt send to chatbot in all messages
+									</FormDescription>
+									<FormControl>
+										<Textarea
+											className="resize-none"
+											placeholder="Prompt..."
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button
+							disabled={!form.formState.isDirty || mutation.isPending}
+							type="submit"
+							className="w-full"
+						>
+							Editar
+						</Button>
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
+	)
+}
+
+export default EditChatbotForm
--- app/dashboard/[chatbotId]/editar/page.tsx
+++ app/dashboard/[chatbotId]/editar/page.tsx
@@ -1,45 +1,17 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
+import { useChatbot } from '@/data/chatbot.client'
+import { useParams } from 'next/navigation'
+import React from 'react'
+import EditChatbotForm from './EditChatbotForm'
+import { Skeleton } from '@/components/ui/skeleton'
 import {
 	Card,
-	CardAction,
 	CardContent,
 	CardDescription,
 	CardHeader,
 	CardTitle,
 } from '@/components/ui/card'
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import {
-	Select,
-	SelectTrigger,
-	SelectValue,
-} from '@/components/ui/select'
-import { Textarea } from '@/components/ui/textarea'
-import { updateChatbot, useChatbot } from '@/data/chatbot.client'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Chatbot } from '@prisma/client'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { useParams } from 'next/navigation'
-import React, { useCallback, useEffect } from 'react'
-import { useForm } from 'react-hook-form'
-import { toast } from 'sonner'
-import { z } from 'zod'
-
-const formSchema = z.object({
-	name: z.string(),
-	initialPrompt: z.string(),
-	model: z.string(),
-})
 
 const EditPage = () => {
 	const params = useParams()
@@ -47,164 +19,44 @@ const EditPage = () => {
 
 	const { data: chatbot } = useChatbot(chatbotId)
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			name: '',
-			initialPrompt: '',
-			model: '',
-		},
-	})
-
-	const setInitialValues = useCallback(() => {
-		if (chatbot) {
-			form.reset({
-				name: chatbot.name,
-				initialPrompt: chatbot.initialPrompt,
-				model: chatbot.model,
-			})
-		}
-	}, [chatbot, form]) // dependencias correctas
-
-	useEffect(() => {
-		setInitialValues()
-	}, [setInitialValues])
-
-	const queryClient = useQueryClient()
-
-	const mutation = useMutation<
-		Chatbot,
-		Error,
-		{
-			chatbotId: string
-			data: z.infer<typeof formSchema>
-		},
-		{ previousChatbot: Chatbot }
-	>({
-		mutationFn: async (data) => {
-			const result = await updateChatbot(data.chatbotId, data.data)
-			return result
-		},
-		onError: (_, data, context) => {
-			toast.error('Error updating chatbot')
-			queryClient.setQueryData(
-				['chatbot', data.chatbotId],
-				context?.previousChatbot
-			)
-		},
-		onMutate: async (data) => {
-			await queryClient.cancelQueries({
-				queryKey: ['chatbot', data.chatbotId],
-			})
-
-			const previousChatbot = queryClient.getQueryData<Chatbot>([
-				'chatbot',
-				data.chatbotId,
-			]) as Chatbot
-
-			queryClient.setQueryData(['chatbot', data.chatbotId], (old: Chatbot) => {
-				if (!old) return old
-				return {
-					...old,
-					...data.data,
-				}
-			})
-
-			return { previousChatbot }
-		},
-		onSuccess: () => {
-			toast.success('Chatbot updated successfully')
-		},
-	})
-
-	const onSubmit = (values: z.infer<typeof formSchema>) => {
-		if (chatbot) {
-			mutation.mutate({ data: values, chatbotId: chatbot.id })
-		}
-	}
-
 	return (
 		<>
-			{chatbot && (
+			{chatbot ? (
+				<EditChatbotForm chatbot={chatbot} />
+			) : (
 				<Card className="md:w-1/2 md:mx-auto">
 					<CardHeader>
-						<CardTitle className="text-xl">Editar {chatbot.name}</CardTitle>
+						<CardTitle className="text-xl">
+							<Skeleton className="h-6 w-40" />
+						</CardTitle>
 						<CardDescription>
-							Crate a chatbot with your own custom settings
+							<Skeleton className="h-4 w-60 mt-1" />
 						</CardDescription>
-						<CardAction>
-							<Button onClick={() => setInitialValues()} size="sm">
-								Reset
-							</Button>
-						</CardAction>
 					</CardHeader>
-					<CardContent>
-						<Form {...form}>
-							<form
-								onSubmit={form.handleSubmit(onSubmit)}
-								className="space-y-6"
-							>
-								<FormField
-									control={form.control}
-									name="name"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Name</FormLabel>
-											<FormDescription>Chatbot public name</FormDescription>
-											<FormControl>
-												<Input placeholder="Chatcito" {...field} />
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="model"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Model</FormLabel>
-											<FormDescription>AI model for chatbot</FormDescription>
-											<Select
-												onValueChange={field.onChange}
-												value={field.value}
-												disabled
-											>
-												<FormControl>
-													<SelectTrigger className="w-full">
-														<SelectValue placeholder="Select a model" />
-													</SelectTrigger>
-												</FormControl>
-											</Select>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="initialPrompt"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Initial Prompt</FormLabel>
-											<FormDescription>
-												Prompt send to chatbot in all messages
-											</FormDescription>
-											<FormControl>
-												<Textarea
-													className="resize-none"
-													placeholder="Prompt..."
-													{...field}
-												/>
-											</FormControl>
-											<FormMessage />
-										</FormItem>
-									)}
-								/>
-								<Button disabled={!form.formState.isDirty || mutation.isPending} type="submit" className="w-full">
-									Editar
-								</Button>
-							</form>
-						</Form>
+					<CardContent className="space-y-6">
+						{/* Name input */}
+						<div className="space-y-2">
+							<Skeleton className="h-4 w-20" /> {/* label */}
+							<Skeleton className="h-10 w-full rounded-md" />
+						</div>
+
+						{/* Model select */}
+						<div className="space-y-2">
+							<Skeleton className="h-4 w-28" /> {/* label */}
+							<Skeleton className="h-10 w-full rounded-md" />
+						</div>
+
+						{/* Initial Prompt textarea */}
+						<div className="space-y-2">
+							<Skeleton className="h-4 w-28" /> {/* label */}
+							<Skeleton className="h-20 w-full rounded-md" />
+						</div>
+
+						{/* Buttons */}
+						<div className="flex gap-2">
+							<Skeleton className="h-10 w-24 rounded-md" />
+							<Skeleton className="h-10 flex-1 rounded-md" />
+						</div>
 					</CardContent>
 				</Card>
 			)}
