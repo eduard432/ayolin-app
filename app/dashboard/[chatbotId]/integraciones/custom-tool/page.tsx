a2599b83ca2035e85627524d0e9b166758cc8a2d
Eduardo <juantokx@gmail.com>
2025-08-04 13:59:22 -0500

    refactor: Clean up form structure and improve readability in CustomToolPage

--- app/dashboard/[chatbotId]/integraciones/custom-tool/page.tsx
+++ app/dashboard/[chatbotId]/integraciones/custom-tool/page.tsx
@@ -55,11 +55,10 @@ const CustomToolPage = () => {
 		},
 	})
 
-
 	useEffect(() => {
 		const subscription = form.watch((value) => {
 			if (value.httpMethod === 'get') {
-        form.setValue('isBodyParams', false)
+				form.setValue('isBodyParams', false)
 			}
 		})
 
@@ -81,153 +80,152 @@ const CustomToolPage = () => {
 				<Form {...form}>
 					<form action="space-y-4">
 						<section className="space-y-4">
-							<Separator />
 							<h3 className="text-current font-semibold text-xl">
 								Información General
 							</h3>
-							<div className="space-y-4">
-								<FormField
-									control={form.control}
-									name="name"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Nombre</FormLabel>
-											<FormDescription>
-												Nombre de la custom tool function
-											</FormDescription>
-											<FormControl>
-												<Input placeholder="Poke API" {...field} />
-											</FormControl>
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="description"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Descripción para IA</FormLabel>
-											<FormDescription>
-												Descripción de la función para que la IA entienda su
-												propósito
-											</FormDescription>
-											<FormControl>
-												<Textarea className="resize-none" {...field} />
-											</FormControl>
-										</FormItem>
-									)}
-								/>
-
-								<Separator />
-								<FormField
-									control={form.control}
-									name="inputSchema"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Input Schema</FormLabel>
-											<FormDescription>
-												Input Schema de los parametros que pasa la AI
-											</FormDescription>
-											<FormControl>
-												<InputSchema {...field} />
-											</FormControl>
-										</FormItem>
-									)}
-								/>
-
-								<Separator />
-								<FormItem>
-									<FormLabel>API URL</FormLabel>
-									<FormDescription>
-										URL donde se ejecutará la función
-									</FormDescription>
-									<FormControl>
-										<div className="flex gap-x-2">
-											<FormField
-												control={form.control}
-												name="httpMethod"
-												render={({ field }) => {
-													const colorMap = {
-														get: 'text-emerald-600',
-														post: 'text-amber-500',
-														put: 'text-blue-600',
-														delete: 'text-red-600',
-													}
-													return (
-														<Select
-															defaultValue={field.value}
-															onValueChange={field.onChange}
+							<FormField
+								control={form.control}
+								name="name"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Nombre</FormLabel>
+										<FormDescription>
+											Nombre de la custom tool function
+										</FormDescription>
+										<FormControl>
+											<Input placeholder="Poke API" {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="description"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Descripción para IA</FormLabel>
+										<FormDescription>
+											Descripción de la función para que la IA entienda su
+											propósito
+										</FormDescription>
+										<FormControl>
+											<Textarea className="resize-none" {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</section>
+						<Separator className="my-4" />
+						<section className="space-y-4">
+							<FormField
+								control={form.control}
+								name="inputSchema"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Input Schema</FormLabel>
+										<FormDescription>
+											Input Schema de los parametros que pasa la AI
+										</FormDescription>
+										<FormControl>
+											<InputSchema {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</section>
+						<Separator className="my-4" />
+						<section className="space-y-4">
+							<FormItem>
+								<FormLabel>API URL</FormLabel>
+								<FormDescription>
+									URL donde se ejecutará la función
+								</FormDescription>
+								<FormControl>
+									<div className="flex gap-x-2">
+										<FormField
+											control={form.control}
+											name="httpMethod"
+											render={({ field }) => {
+												const colorMap = {
+													get: 'text-emerald-600',
+													post: 'text-amber-500',
+													put: 'text-blue-600',
+													delete: 'text-red-600',
+												}
+												return (
+													<Select
+														defaultValue={field.value}
+														onValueChange={field.onChange}
+													>
+														<SelectTrigger
+															className={cn(
+																'font-semibold w-[120px]',
+																colorMap[field.value]
+															)}
 														>
-															<SelectTrigger
-																className={cn(
-																	'font-semibold w-[120px]',
-																	colorMap[field.value]
-																)}
+															<SelectValue placeholder="HTTP METHOD" />
+														</SelectTrigger>
+														<SelectContent className="font-semibold">
+															<SelectItem
+																className={cn(colorMap['get'])}
+																value="get"
 															>
-																<SelectValue placeholder="HTTP METHOD" />
-															</SelectTrigger>
-															<SelectContent className="font-semibold">
-																<SelectItem
-																	className={cn(colorMap['get'])}
-																	value="get"
-																>
-																	GET
-																</SelectItem>
-																<SelectItem
-																	className={cn(colorMap['post'])}
-																	value="post"
-																>
-																	POST
-																</SelectItem>
-																<SelectItem
-																	className={cn(colorMap['put'])}
-																	value="put"
-																>
-																	PUT
-																</SelectItem>
-																<SelectItem
-																	className={cn(colorMap['delete'])}
-																	value="delete"
-																>
-																	DELETE
-																</SelectItem>
-															</SelectContent>
-														</Select>
-													)
-												}}
-											/>
+																GET
+															</SelectItem>
+															<SelectItem
+																className={cn(colorMap['post'])}
+																value="post"
+															>
+																POST
+															</SelectItem>
+															<SelectItem
+																className={cn(colorMap['put'])}
+																value="put"
+															>
+																PUT
+															</SelectItem>
+															<SelectItem
+																className={cn(colorMap['delete'])}
+																value="delete"
+															>
+																DELETE
+															</SelectItem>
+														</SelectContent>
+													</Select>
+												)
+											}}
+										/>
 
-											<FormField
-												control={form.control}
-												name="apiUrl"
-												render={({ field }) => (
-													<Input
-														placeholder="https://localhost:4000/api/v1/poke-api"
-														{...field}
-													/>
-												)}
-											/>
-										</div>
-									</FormControl>
-								</FormItem>
-
-								<FormField
-									control={form.control}
-									name="isBodyParams"
-									render={({ field }) => (
-										<FormItem className="flex gap-x-4 justify-items-center" >
-											<FormControl>
-												<Switch
-                          disabled={form.watch('httpMethod') === 'get'}
-													checked={field.value}
-													onCheckedChange={field.onChange}
+										<FormField
+											control={form.control}
+											name="apiUrl"
+											render={({ field }) => (
+												<Input
+													placeholder="https://localhost:4000/api/v1/poke-api"
+													{...field}
 												/>
-											</FormControl>
-											<Label>Usar Body Params</Label>
-										</FormItem>
-									)}
-								/>
-							</div>
+											)}
+										/>
+									</div>
+								</FormControl>
+							</FormItem>
+
+							<FormField
+								control={form.control}
+								name="isBodyParams"
+								render={({ field }) => (
+									<FormItem className="flex gap-x-4 justify-items-center">
+										<FormControl>
+											<Switch
+												disabled={form.watch('httpMethod') === 'get'}
+												checked={field.value}
+												onCheckedChange={field.onChange}
+											/>
+										</FormControl>
+										<Label>Usar Body Params</Label>
+									</FormItem>
+								)}
+							/>
 						</section>
 					</form>
 				</Form>
