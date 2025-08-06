52dfa63105b2aef344395239d27b13473fe9cbeb
LeoDuffed <carlosmroder@gmail.com>
2025-08-05 19:35:33 -0600

    cambios en la pagine de los posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "nodejs"
+
 import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
@@ -7,34 +9,55 @@ import { notFound } from "next/navigation"
 import NavbarBLog from "@/components/navbarBlog"
 
 interface PostPageProps {
-    params: {slug: string}
+  params: { slug: string }
 }
 
-export default async function PostPage({ params }: PostPageProps){
-    const postsDirectory = path.join(process.cwd(), "posts")
-    const filePath = path.join(postsDirectory, `${params.slug}.md`)
+export default async function PostPage({ params }: PostPageProps) {
+  const postsDirectory = path.join(process.cwd(), "posts")
 
-    if(!fs.existsSync(filePath)){
-        return notFound()
-    }
+  if (!fs.existsSync(postsDirectory)) {
+    notFound()
+  }
 
-    const fileContent = fs.readFileSync(filePath, "utf8")
-    const { data, content } = matter(fileContent)
+  const filePath = path.join(postsDirectory, `${params.slug}.md`)
 
-    const processContent = await remark().use(html).process(content)
-    const contentHtml = processContent.toString()
+  if(!fs.existsSync(filePath)){
+    notFound()
+  }
 
-    return (
-        <>
-            <NavbarBLog/>
-            <article className="px-6 md:px-20 py-16 mx-w-4xl min-h-screen mx-auto pt-40 bg-gradient-to-bl from-cyan-900 from-10% via-gray-900 via-40% to-black">
-                <h1 className="text-3xl md:text-4xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-                    {data.title}
-                </h1>
-                <p className="text-sm text-muted-foreground mb-8">{data.date}</p>
+  const fileContent = fs.readFileSync(filePath, "utf8")
+  const { data, content } = matter(fileContent)
 
-                <div className="prose prose-lg prose-invert text-white text-lg" dangerouslySetInnerHTML={{ __html: contentHtml }} />
-            </article>
-        </>
-    )
+  const processedContent = await remark().use(html).process(content)
+  const contentHtml = processedContent.toString()
+
+  return (
+    <>
+      <NavbarBLog />
+      <article className="px-6 md:px-20 py-16 mx-w-4xl min-h-screen mx-auto pt-40 bg-gradient-to-bl from-cyan-900 from-10% via-gray-900 via-40% to-black">
+        <h1 className="text-3xl md:text-4xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
+          {data.title}
+        </h1>
+        <p className="text-sm text-muted-foreground mb-8">{data.date}</p>
+        <div
+          className="prose prose-lg prose-invert text-white text-lg"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        />
+      </article>
+    </>
+  )
+}
+
+export async function generateStaticParams() {
+  const postsDirectory = path.join(process.cwd(), "posts")
+
+  if (!fs.existsSync(postsDirectory)) {
+    return [] // Si no hay carpeta, no hay rutas
+  }
+
+  const filenames = fs.readdirSync(postsDirectory)
+
+  return filenames.map((filename) => ({
+    slug: filename.replace(/\.md$/, ""),
+  }))
 }
