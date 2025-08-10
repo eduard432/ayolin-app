235a987273959b2d2d4433de9251dc88fc0b0a9f
LeoDuffed <carlosmroder@gmail.com>
2025-08-10 01:06:41 -0600

    Ya ninguna extencion tiene la foto de Morty jijiji

--- app/dashboard/[chatbotId]/integraciones/marketplace/categoria/[category]/page.tsx
+++ app/dashboard/[chatbotId]/integraciones/marketplace/categoria/[category]/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { integrationsData } from '@/lib/integrationsData'
 import React from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
+import { Puzzle } from 'lucide-react'
+import { cn } from '@/lib/utils'
 import { Card, CardContent } from '@/components/ui/card'
 import { useParams, useRouter } from 'next/navigation'
 
@@ -24,9 +26,14 @@ const Page = () => {
 				>
 					<CardContent className="flex justify-between items-center">
 						<div className="flex items-center gap-x-4">
-							<Avatar>
-								<AvatarImage src="https://github.com/shadcn.png" />
-								<AvatarFallback>CN</AvatarFallback>
+							<Avatar className='w-10 h-10 ring-1 ring-blue-300/20'>
+								<AvatarFallback
+									className={cn(
+										"w-full h-full flex items-center justify-center rounded-full bg-blue-500/10 text-blue-400"
+									)}
+								>
+									<Puzzle className='h-5 w-5'/>
+								</AvatarFallback>
 							</Avatar>
 							<div>
 								<h4 className="font-medium">{integration.name}</h4>
