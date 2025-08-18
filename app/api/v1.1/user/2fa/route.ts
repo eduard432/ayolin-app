926df92caad76352083c6e4de6015c3b9409bd2e
Eduardo <juantokx@gmail.com>
2025-08-18 08:49:36 -0600

    fix: api http method

--- app/api/v1.1/user/2fa/route.ts
+++ app/api/v1.1/user/2fa/route.ts
@@ -3,7 +3,7 @@ import { ApiErrorHandler } from '@/lib/api/ApiError'
 import { ApiResponse } from '@/lib/api/ApiResponse'
 import { ApiValidator } from '@/lib/api/ApiValidate'
 
-export const PUT = ApiErrorHandler.wrapAuth(async (request, _, session) => {
+export const PATCH = ApiErrorHandler.wrapAuth(async (request, _, session) => {
 	const data = await ApiValidator.validateBody(request, update2FASchema)
 	const user = await update2FA(data.enabled, session.user.id)
 	return ApiResponse.updated(user, 'User updated')
