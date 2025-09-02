dfc68ff6d9d9f50930ea6365e1e3249745f78b8c
eduard432 <juantokx@gmail.com>
2025-09-01 20:28:03 -0600

    Refactor form schema and update ToolFunctionType to include 'native'

--- app/dashboard/admin/tools/page.tsx
+++ app/dashboard/admin/tools/page.tsx
@@ -29,34 +29,24 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-const formSchemaCommon = {
+const formSchema = z.object({
 	name: z.string(),
-	blogDescription: z.string(),
+	shortDesc: z.string(),
+	description: z.string(),
 	settingsSchema: fieldSchema.array().optional(),
-}
-
-const formSchemaUnion = z.discriminatedUnion('type', [
-	z.object({
-		...formSchemaCommon,
-		type: z.literal('code'),
-	}),
-	z.object({
-		...formSchemaCommon,
-		type: z.literal('fetch'),
-		description: z.string(),
-		apiUrl: z.string(),
-		inputSchema: fieldSchema.array(),
-	}),
-])
+	inputSchema: fieldSchema.array(),
+	apiUrl: z.string(),
+	isNative: z.boolean(),
+})
 
 const ToolsPage = () => {
-	const [isFetchTool, setIsFetchTool] = useState(false)
 	const [isSettingsSchema, setIsSettingSchema] = useState(false)
 
-	const form = useForm<z.infer<typeof formSchemaUnion>>({
-		resolver: zodResolver(formSchemaUnion),
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
 		defaultValues: {
 			name: '',
+			isNative: false,
 		},
 	})
 
@@ -124,7 +114,23 @@ const ToolsPage = () => {
 							</div>
 							<FormField
 								control={form.control}
-								name="blogDescription"
+								name="shortDesc"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Descripción Corta</FormLabel>
+										<FormDescription>
+											Descripción corta que se mostrara a los usuarios en la
+											página de marketplace
+										</FormDescription>
+										<FormControl>
+											<Textarea className="resize-none" {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="description"
 								render={({ field }) => (
 									<FormItem>
 										<FormLabel>Descripción</FormLabel>
@@ -140,72 +146,82 @@ const ToolsPage = () => {
 							/>
 						</section>
 						<Separator />
-						<section className="flex items-center gap-x-4">
-							<Switch
-								checked={isFetchTool}
-								onCheckedChange={(v) => setIsFetchTool(v)}
-								id="tool-mode"
+						<section className="space-y-4">
+							<h3 className="text-current font-semibold text-xl">
+								Fetch Tool Config
+							</h3>
+							<FormField
+								control={form.control}
+								name="description"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Descripción para IA</FormLabel>
+										<FormDescription>
+											Descripción que sera enviada al modelo de IA para que
+											entienda el uso de la función
+										</FormDescription>
+										<FormControl>
+											<Textarea className="resize-none" {...field} />
+										</FormControl>
+									</FormItem>
+								)}
 							/>
-							<Label htmlFor="tool-mode">Fetch Tool</Label>
+							<FormField
+								control={form.control}
+								name="apiUrl"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Api Url</FormLabel>
+										<FormDescription>
+											Api url donde se ejecutara la función
+										</FormDescription>
+										<FormControl>
+											<Input
+												placeholder="https://localhost:4000/api/v1/poke-api"
+												{...field}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<Separator />
+							<FormField
+								control={form.control}
+								name="inputSchema"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Input Schema</FormLabel>
+										<FormDescription>
+											Input Schema de los parametros que pasa la AI
+										</FormDescription>
+										<FormControl>
+											<InputSchema {...field} />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
 						</section>
 						<Separator />
-						{isFetchTool && (
-							<section className="space-y-4">
-								<h3 className="text-current font-semibold text-xl">
-									Fetch Tool
-								</h3>
-								<FormField
-									control={form.control}
-									name="description"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Descripción para IA</FormLabel>
-											<FormDescription>
-												Descripción que sera enviada al modelo de IA para que
-												entienda el uso de la función
-											</FormDescription>
-											<FormControl>
-												<Textarea className="resize-none" {...field} />
-											</FormControl>
-										</FormItem>
-									)}
-								/>
-								<FormField
-									control={form.control}
-									name="apiUrl"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Api Url</FormLabel>
-											<FormDescription>
-												Api url donde se ejecutara la función
-											</FormDescription>
-											<FormControl>
-												<Input
-													placeholder="https://localhost:4000/api/v1/poke-api"
-													{...field}
-												/>
-											</FormControl>
-										</FormItem>
-									)}
-								/>
-								<Separator />
-								<FormField
-									control={form.control}
-									name="inputSchema"
-									render={({ field }) => (
-										<FormItem>
-											<FormLabel>Input Schema</FormLabel>
-											<FormDescription>
-												Input Schema de los parametros que pasa la AI
-											</FormDescription>
-											<FormControl>
-												<InputSchema {...field} />
-											</FormControl>
-										</FormItem>
-									)}
-								/>
-							</section>
-						)}
+						<section className="flex items-center gap-x-4">
+							<FormField
+								control={form.control}
+								name="isNative"
+								render={({ field }) => (
+									<FormItem className="flex items-center gap-x-4">
+										<FormControl>
+											<Switch
+												id="is-native"
+												checked={field.value}
+												onCheckedChange={field.onChange}
+											/>
+										</FormControl>
+										<Label htmlFor="is-native">
+											{field.value ? 'Tool Nativa' : 'Tool Externa'}
+										</Label>
+									</FormItem>
+								)}
+							/>
+						</section>
 						<Separator />
 						<section className="flex items-center gap-x-4">
 							<Switch
