f33cca46c49e415aca47975f4a443280e334816a
Leonardo Mrtz <carlosmroder@gmail.com>
2025-12-18 19:44:19 -0600

    estoy arreglando los componentes relacionados con el login,

---
+++ route.ts
@@ -0,0 +1,44 @@
+/**
+ * Centralized routes configuration
+ */
+export const ROUTES: {
+	publicExact: Set<string>
+	publicPatterns: RegExp[]
+	auth: Set<string>
+	apiAuthPrefix: string
+	redirect: {
+		login: string
+		afterLogin: string
+	}
+} = {
+	publicExact: new Set([
+		'/',
+		'/auth/new-verification',
+		'/condiciones-servicios',
+		'/politica-privacidad',
+	]),
+
+	publicPatterns: [
+		/^\/api\/v1\/chat\/[^/]+$/, // /api/v1/chat/:chatId
+	],
+
+	auth: new Set([
+		'/auth/login',
+		'/auth/register',
+		'/auth/error',
+		'/auth/reset',
+		'/auth/new-password',
+	]),
+
+	apiAuthPrefix: '/api/v1/auth',  // esto hay que revisarlo
+
+	redirect: {
+		login: '/auth/login',
+		afterLogin: '/dashboard/general',
+	},
+}
+
+/**
+ * Authentication callback routes (under apiAuthPrefix)
+ */
+export const authCallbacks = new Set(['/callback/google', '/callback/github'])
